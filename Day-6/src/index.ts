687a41d8489dc66dc966c14ab268c7fb9be5fd25
Grant Ralls <grant.ralls@ferguson.com>
2022-12-06 12:28:26 -0500

    refactoring day 6

--- Day-6/src/index.ts
+++ Day-6/src/index.ts
@@ -2,55 +2,34 @@ import { readFile } from 'fs/promises';
 
 readFile('src/input.txt', 'utf-8').then((data) => {
     const splitDataStream = data.split('');
-    const answerOne = getStartOfIndex([...splitDataStream], getPacket);
+    const answerOne = getStartOfIndex([...splitDataStream], 4);
     console.log(`Answer one: ${answerOne}`);
 
-    const answerTwo = getStartOfIndex([...splitDataStream], getMessage);
+    const answerTwo = getStartOfIndex([...splitDataStream], 14);
     console.log(`Answer two: ${answerTwo}`);
 });
 
-const getStartOfIndex = (
-    datastream: string[],
-    callback: (
-        acc: number,
-        currentValue: string,
-        currentIndex: number,
-        datastream: string[]
-    ) => number
-) => datastream.reduce(callback, 0);
+const getStartOfIndex = (datastream: string[], numberOfUniqueValues: number) =>
+    datastream.reduce(
+        (
+            acc: number,
+            _: string,
+            currentIndex: number,
+            datastream: string[]
+        ) => {
+            const arrayToCheck = datastream.slice(
+                currentIndex,
+                currentIndex + numberOfUniqueValues
+            );
 
-const getPacket = (
-    acc: number,
-    _: string,
-    currentIndex: number,
-    datastream: string[]
-) => {
-    const arrayToCheck = datastream.slice(currentIndex, currentIndex + 4);
+            const mySet = new Set(arrayToCheck);
 
-    const mySet = new Set(arrayToCheck);
+            if (mySet.size === numberOfUniqueValues) {
+                datastream.splice(1);
+                return currentIndex + numberOfUniqueValues;
+            }
 
-    if (mySet.size === 4) {
-        datastream.splice(1);
-        return currentIndex + 4;
-    }
-
-    return acc;
-};
-
-const getMessage = (
-    acc: number,
-    _: string,
-    currentIndex: number,
-    datastream: string[]
-) => {
-    const arrayToCheck = datastream.slice(currentIndex, currentIndex + 14);
-
-    const mySet = new Set(arrayToCheck);
-
-    if (mySet.size === 14) {
-        datastream.splice(1);
-        return currentIndex + 14;
-    }
-
-    return acc;
-};
+            return acc;
+        },
+        0
+    );
