2f3ab85ba73992b699a01936e2bf55955c1e63a4
Grant Ralls <grant.ralls@ferguson.com>
2022-12-07 09:54:38 -0500

    removing unused describe

--- Day-6/src/utils/utils.test.ts
+++ Day-6/src/utils/utils.test.ts
@@ -1,35 +1,33 @@
 import { getStartOfIndex } from './utils';
 
-describe('Question 1', () => {
-    describe('getStartOfIndex', () => {
-        it('should return 0 for aaaaaaaaaaa', () => {
-            const result = getStartOfIndex('aaaaaaaaaaa', 4);
+describe('getStartOfIndex', () => {
+    it('should return 0 for aaaaaaaaaaa', () => {
+        const result = getStartOfIndex('aaaaaaaaaaa', 4);
 
-            expect(result).toBe(0);
-        });
+        expect(result).toBe(0);
+    });
 
-        it('should return 5 for bvwbjplbgvbhsrlpgdmjqwftvncz', () => {
-            const result = getStartOfIndex('bvwbjplbgvbhsrlpgdmjqwftvncz', 4);
+    it('should return 5 for bvwbjplbgvbhsrlpgdmjqwftvncz', () => {
+        const result = getStartOfIndex('bvwbjplbgvbhsrlpgdmjqwftvncz', 4);
 
-            expect(result).toBe(5);
-        });
+        expect(result).toBe(5);
+    });
 
-        it('should return 6 for nppdvjthqldpwncqszvftbrmjlhg', () => {
-            const result = getStartOfIndex('nppdvjthqldpwncqszvftbrmjlhg', 4);
+    it('should return 6 for nppdvjthqldpwncqszvftbrmjlhg', () => {
+        const result = getStartOfIndex('nppdvjthqldpwncqszvftbrmjlhg', 4);
 
-            expect(result).toBe(6);
-        });
+        expect(result).toBe(6);
+    });
 
-        it('should return 10 for nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', () => {
-            const result = getStartOfIndex('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4);
+    it('should return 10 for nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', () => {
+        const result = getStartOfIndex('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4);
 
-            expect(result).toBe(10);
-        });
+        expect(result).toBe(10);
+    });
 
-        it('should return 11 for zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', () => {
-            const result = getStartOfIndex('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4);
+    it('should return 11 for zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', () => {
+        const result = getStartOfIndex('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4);
 
-            expect(result).toBe(11);
-        });
+        expect(result).toBe(11);
     });
 });
